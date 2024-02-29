d07c2d655a8089ba8f69dc8b50806bad1f47214d
Marc Vindel Marsal <146850360+mvindel-dev@users.noreply.github.com>
2024-02-29 08:12:25 +0100

    Added Fontawesome

--- projects/rescue-pets-mobile-app/src/app/app.module.ts
+++ projects/rescue-pets-mobile-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { IonicModule } from '@ionic/angular';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    IonicModule.forRoot({})
+    IonicModule.forRoot({}),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
