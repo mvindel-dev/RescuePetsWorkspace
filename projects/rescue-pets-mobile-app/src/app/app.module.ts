d56f86273fd6fefb0f29944af4c23254677d730b
Marc Vindel Marsal <146850360+mvindel-dev@users.noreply.github.com>
2024-03-01 16:34:41 +0100

    Revert "Added Fontawesome"
    This reverts commit d07c2d655a8089ba8f69dc8b50806bad1f47214d.

--- projects/rescue-pets-mobile-app/src/app/app.module.ts
+++ projects/rescue-pets-mobile-app/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -13,8 +12,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    IonicModule.forRoot({}),
-    FontAwesomeModule
+    IonicModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
