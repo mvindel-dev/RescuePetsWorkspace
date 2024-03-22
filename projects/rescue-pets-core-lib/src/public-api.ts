363cc60682e3f67262bd7538500ef694130fe493
Marc Vindel Marsal <146850360+mvindel-dev@users.noreply.github.com>
2024-03-22 09:32:22 +0100

    modded exporting services, guards and models

--- projects/rescue-pets-core-lib/src/public-api.ts
+++ projects/rescue-pets-core-lib/src/public-api.ts
@@ -2,12 +2,17 @@
  * Public API Surface of rescue-pets-core-lib
  */
 
+export * from './lib/guards/auth.guard';
+
 export * from './lib/models/animal/animal';
 export * from './lib/models/shelter/shelter';
-export * from './lib/models/userModel';
+export * from './lib/models/user.model';
+export * from './lib/models/reserve/reserve-animal';
+
 export * from './lib/services/auth.service';
 export * from './lib/services/Animals/animals.service';
 export * from './lib/services/Pets/pet.service';
 export * from './lib/services/Shelter/shelter.service';
+export * from './lib/services/Volunteer/volunteer.service';
 
 
